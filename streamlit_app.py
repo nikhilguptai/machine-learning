26179b59751228d53917bcd98761ce38fba3eed7
nikhilguptai <157030666+nikhilguptai@users.noreply.github.com>
2024-09-06 11:18:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import seaborn as sns
+import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
