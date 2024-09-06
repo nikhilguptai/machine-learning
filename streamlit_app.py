3c79846c3d848b7d0748b0b0994cfaf76c087ec9
nikhilguptai <157030666+nikhilguptai@users.noreply.github.com>
2024-09-06 07:30:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ st.title('🤖This is Machine Learning app')
 
 st.info('This app builds machine learning model')
 with st.expander('**Raw data**'):
- df=pd,read_csv('NFLX.csv')
+ df=pd.read_csv('NFLX.csv')
  df
