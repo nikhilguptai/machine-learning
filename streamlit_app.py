9ee7e508e5e4fce3ab32982679df619f8d02fb5a
nikhilguptai <157030666+nikhilguptai@users.noreply.github.com>
2024-09-06 07:34:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,10 @@ st.info('This app builds machine learning model')
 with st.expander('**Raw data**'):
  df=pd.read_csv('NFLX.csv')
  df
+ st.write('**X**')
+ df['Date'] = pd.to_datetime(df['Date'])
+ df['year'] = df['Date'].dt.year
+ df['month'] = df['Date'].dt.month
+ df['day'] = df['Date'].dt.day
+ Xx=df.drop('Date',axis=1,inplace=True)
+ X=Xx.drop('Close',axis=1)
