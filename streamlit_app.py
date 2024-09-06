fb7d0aceac19e88b5acee66b6ccdf1d60d9b140c
nikhilguptai <157030666+nikhilguptai@users.noreply.github.com>
2024-09-06 07:28:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import pandas as pd
 
 st.title('🤖This is Machine Learning app')
 
 st.info('This app builds machine learning model')
+with st.exoander('**Raw data**')
+ df=pd,read_csv('NFLX.csv')
+ df
