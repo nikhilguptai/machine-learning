d6035200c393e52dc053fc12b6ed5ae426d3396d
nikhilguptai <157030666+nikhilguptai@users.noreply.github.com>
2024-09-07 17:07:21 +0530

    Update Electronics Recommendation.py

--- pages/Electronics Recommendation.py
+++ pages/Electronics Recommendation.py
@@ -6,3 +6,4 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
+df=pd.read_csv("
