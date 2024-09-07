b100dc2e817ed6cc0137ade609eff6d6b4eb4289
nikhilguptai <157030666+nikhilguptai@users.noreply.github.com>
2024-09-07 09:21:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,13 @@ st.title('🤖This is Machine Learning app')
 
 st.info('This app builds machine learning model')
 st.markdown("**This app consist of Five Prediction model thats are:-**")
+st.write('1 :- Netflix stock Prediction')
+st.write('2 :- Concrete strenght Prediction')
+st.write('3 :- Netflix stock Prediction')
+st.write('4 :- Netflix stock Prediction')
+st.write('5 :- Netflix stock Prediction')
+
+
 
 
 
