420c897797156b2277af0d2672af5318cb55e485
nikhilguptai <157030666+nikhilguptai@users.noreply.github.com>
2024-09-07 17:34:06 +0530

    Update Electronics Recommendation.py

--- pages/Electronics Recommendation.py
+++ pages/Electronics Recommendation.py
@@ -6,4 +6,5 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
-df=pd.read_csv("
+df=pd.read_csv('kaggle datasets download -d datafiniti/electronic-products-prices')
+df
