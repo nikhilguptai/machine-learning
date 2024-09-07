3a27a5f238991784cfeb1e31aad7e7ca8face463
nikhilguptai <157030666+nikhilguptai@users.noreply.github.com>
2024-09-08 03:40:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,10 +11,10 @@ st.info('This app builds machine learning model')
 st.info('Please select any one from Sidebar which you want to Predict')
 st.markdown("**This app consist of Five Prediction model thats are:-**")
 st.write('1 :- Concrete strenght Prediction')
-st.write('2 :- Electronics Recommendation strenght Prediction')
+
 st.write('3 :- Netflix stock Prediction')
 st.write('4 :- Patient Survival Prediction')
-st.write('5 :- Social Network Ads purchased Prediction')
+st.write('**Social Network Ads purchased Prediction**')
 
 
 
