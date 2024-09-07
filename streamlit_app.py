11e1d82a5799a2dfde8c743ecbb8e80b6725bb69
nikhilguptai <157030666+nikhilguptai@users.noreply.github.com>
2024-09-07 07:50:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,10 @@ import stremlit as st
 st.set_page_config(
     page_title="Multi-Page App",
     page_icon=":rocket:",
+)
+st.title('🤖This is Machine Learning app')
+
+st.info('This app builds machine learning model')
 
 
 
