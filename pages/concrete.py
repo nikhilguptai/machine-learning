3cac088a8741cb375befd92db8c72a7a5c996271
nikhilguptai <157030666+nikhilguptai@users.noreply.github.com>
2024-09-07 07:45:38 +0530

    Create concrete.py

---
+++ pages/concrete.py
@@ -0,0 +1,85 @@
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+
+
+st.title('🤖This is Machine Learning app')
+
+st.info('This app predict concrete strenght')
+
+with st.expander('**Raw data**'):
+ df=pd.read_csv('concrete_data.csv')
+ df
+ st.write('**X**')
+
+ X = df.filter(items=[col for col in df.columns if col not in ['concrete_compressive_strength']])
+ X
+ st.write('**Y**')
+ Y = df.concrete_compressive_strength
+ Y
+with st.expander('Data Visualization'):
+ st.write('***Data **')
+ corr = df.corr()
+ sns.heatmap(corr,annot=True,cmap='coolwarm',cbar=True)
+ sns.pairplot(df)
+ for i in df.columns:
+  plt.figure(figsize=(10,6))
+  sns.distplot(df[i])
+  plt.title(i)
+  st.pyplot(plt)
+  
+Open = st.text_input("Enter Open:")
+High = st.text_input("Enter High:")
+Low = st.text_input("Enter Low:")
+Adj_Close = st.text_input("Enter Adj_Close:") 
+Volume = st.text_input("Enter Volume:")
+year = st.text_input("Enter year:")
+month = st.text_input("Enter month:")
+day = st.text_input("Enter Day:")
+input_data = np.array([[float(Open), float(High), float(Low),float(Adj_Close),float(Volume),float(year),float(month),float(day)]])
+
+#data frame
+data = { 'Open' : Open,
+        'High' : High,
+        'Low' : Low,
+        'Adj_Close' : Adj_Close,
+        'Volume' : Volume,
+        'year' : year,
+        'month' : month,
+        'day' : day,}
+
+input_df = pd.DataFrame(data, index=[0])
+input_stocks = pd.concat([input_df, X], axis=0)
+with st.expander('Input features'):
+  st.write('**Input data**')
+  input_df
+  st.write('**Combined stocks data**')
+  input_stocks
+ 
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2)
+
+model = LinearRegression()
+
+model.fit(X_train, Y_train)
+
+Y_pred = model.predict(X_test)
+
+r2 = r2_score(Y_test, Y_pred)
+
+st.write("R-squared (R2):", r2)
+st.write(f"Input shape: {input_data.shape}")
+
+# Make the prediction when the user clicks a button
+if st.button('Predict'):
+    try:
+        prediction = model.predict(input_data)
+        # Display the prediction
+        st.write("Prediction:", prediction)
+    except ValueError as e:
+        st.error(f"Error during prediction: {e}")
+
