4624ea4e53230e7267c21194c164a4b14219d649
nikhilguptai <157030666+nikhilguptai@users.noreply.github.com>
2024-09-08 00:38:01 +0530

    Update Popurali Recommendation.py

--- pages/Popurali Recommendation.py
+++ pages/Popurali Recommendation.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
-movies = pd.read_csv('tmdb_5000_movies.csv')
-movies = movies[['title','overview','popularity','vote_count']]
+movies = pd.read_csv('/kaggle/input/tmdb-movie-metadata/tmdb_5000_movies.csv')
+credits = pd.read_csv('/kaggle/input/tmdb-movie-metadata/tmdb_5000_credits.csv') 
 movies
 
