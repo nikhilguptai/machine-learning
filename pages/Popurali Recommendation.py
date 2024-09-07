d6f3a6a239b628c942c6fc9807b8644fbc16def5
nikhilguptai <157030666+nikhilguptai@users.noreply.github.com>
2024-09-08 02:16:05 +0530

    Update Popurali Recommendation.py

--- pages/Popurali Recommendation.py
+++ pages/Popurali Recommendation.py
@@ -6,7 +6,20 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
-movies = pd.read_csv('DatafinitiElectronicsProductsPricingData.csv')
 
-movies
+import gdown
+
+file_id = '1T8v5XIHM4Tsq-18MJsTYWBmmvg18-pR9'
+
+# Google Drive download link
+url = f'https://drive.google.com/uc?id={file_id}'
+
+# Output file path
+output = 'DatafinitiElectronicsProductsPricingData.csv'
+
+# Download the dataset from Google Drive
+gdown.download(url, output, quiet=False)
+
+# Load the dataset into a pandas DataFrame
+movies = pd.read_csv(output)
 
