9645ecac3abf7be06e968aed39d93e20e0b96f29
nikhilguptai <157030666+nikhilguptai@users.noreply.github.com>
2024-09-08 10:54:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,16 @@ st.set_page_config(
     page_icon=":rocket:",
 )
 st.title('🤖This is Machine Learning app')
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background-color: #f0f0f5;  /* Light gray background */
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 st.info('This app builds machine learning model')
 st.info('Please select any one from Sidebar which you want to Predict')
