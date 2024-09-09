82f0ccfa10f8065af6bacc7f822c5840bf2bb709
nikhilguptai <157030666+nikhilguptai@users.noreply.github.com>
2024-09-10 03:23:42 +0530

    Update electronic.py

--- pages/electronic.py
+++ pages/electronic.py
@@ -1,27 +1,21 @@
-import requests
+import gdown
 import pandas as pd
 import streamlit as st
 
 # Direct download URL
-file_id = '1T8v5XIHM4Tsq-18MJsTYWBmmvg18-pR9'
-url = f'https://drive.google.com/uc?export=download&id={file_id}'
+url = 'https://drive.google.com/uc?export=download&id=1T8v5XIHM4Tsq-18MJsTYWBmmvg18-pR9'
 output = 'DatafinitiElectronicsProductsPricingData.csv'
 
 try:
     # Download the file
-    response = requests.get(url)
-    response.raise_for_status()  # Check for request errors
-
-    # Save the file
-    with open(output, 'wb') as f:
-        f.write(response.content)
-    
+    gdown.download(url, output, quiet=False)
     st.write("Download completed successfully!")
 
     # Read the dataset into a DataFrame
     df = pd.read_csv(output)
     df
 
-
+except Exception as e:
+    st.error(f"An error occurred: {e}")
 
 
